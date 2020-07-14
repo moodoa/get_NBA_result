ed0042170ccb9bdacf7dad543cc19f2ae27ecd1d
weed <aa469413927@gmail.com>
2020-07-14 15:13:43 +0800

    <fix> update dataframe form

--- get_NBA_result.py
+++ get_NBA_result.py
@@ -5,6 +5,11 @@ def get_NBA_result(game_id):
 #     get result(score)
     html = soup.find(name='div',attrs={'class':'competitors'})
     game_result = pd.read_html(str(html))[0]
+    col = game_result.columns.to_list()
+    col[0] = 'Team'
+    game_result.columns = col
+#     get article title
+    title = '[BOX] '+game_result['Team'][0]+' '+str(game_result['T'][0])+' : '+str(game_result['T'][1])+' '+game_result['Team'][1]
 #     get stat(players stat)
     stat = soup.find(name='div',attrs={'id':'gamepackage-boxscore-module'})
     for i in soup.find_all(name='span',attrs={'class':'abbr'}):
@@ -21,5 +26,15 @@ def get_NBA_result(game_id):
     column[0] = 'Players'
     team1.columns = column
     team2.columns = column
+    for c in team1.columns[1:]:
+        for i in team1[c].values:
+            if len(str(i))>=10:
+                team1[c].replace({i: 0}, inplace=True)
+        for i in team2[c].values:
+            if len(str(i))>=10:
+                team2[c].replace({i: 0}, inplace=True)
+
+    team1.fillna(' ',inplace=True)
+    team2.fillna(' ',inplace=True)
     
-    return game_result,team1,team2
+    return title,game_result,team1,team2
