04c2bac1689e428b0082c308cba905bf051cfe13
weed <aa469413927@gmail.com>
2020-07-23 11:20:42 +0800

    <add> add get_NBA_result_with_conditions function

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 * 以 `gameid` 為參數爬取 NBA 對戰組合的戰績以及球員數據。
 * 回傳值有四項，分別是 1,`標題(比分)`、2,`各節分數`、3,`A隊數據`、4,`B隊數據`。
 * `po_dcard`、`daily_post` 這兩個 function 為使用 selenium 的方式 PO 文，若無 Dacrd Token 可使用此方式。
-
+* `get_NBA_result_with_conditions` 此 function 與 `get_NBA_result` 用法相同，但回傳值會保留球員狀態，e.g. `DNP-COACH'S DECISION`。
 #### df_to_png.py
 * 將爬取下來的 `Datafrme` 轉成 `png` 圖檔，並存在本地資料夾中。
 
--- po_dcard_functions/get_NBA_result.py
+++ po_dcard_functions/get_NBA_result.py
@@ -11,37 +11,37 @@ def get_NBA_result(game_id):
     web = requests.get(f'https://www.espn.com/nba/boxscore?gameId={game_id}')
     content = web.content
     soup = BeautifulSoup(content,'html.parser')
-#     get result(score)
+    #     get result(score)
     html = soup.find(name='div',attrs={'class':'competitors'})
     game_result = pd.read_html(str(html))[0]
     col = game_result.columns.to_list()
     col[0] = 'Team'
     game_result.columns = col
-#     get article title
+    #     get article title
     title = '[BOX] '+game_result['Team'][0]+' '+str(game_result['T'][0])+' : '+str(game_result['T'][1])+' '+game_result['Team'][1]
-#     get stat(players stat)
+    #     get stat(players stat)
     stat = soup.find(name='div',attrs={'id':'gamepackage-boxscore-module'})
-    for i in soup.find_all(name='span',attrs={'class':'abbr'}):
-        i.decompose()
-    for i in stat.find_all(name='span',attrs={'class':'position'}):
-        i.insert_before('/')
+    for abbr in soup.find_all(name='span',attrs={'class':'abbr'}):
+        abbr.decompose()
+    for position in stat.find_all(name='span',attrs={'class':'position'}):
+        position.insert_before('/')
 
     team1 = pd.read_html(str(stat))[0]
     team2 = pd.read_html(str(stat))[1]
-    col = team1.columns.tolist()
+    column_list = team1.columns.tolist()
     column = []
-    for i,j in col:
-        column.append(i)
+    for col_a,col_b in column_list:
+        column.append(col_a)
     column[0] = 'Players'
     team1.columns = column
     team2.columns = column
-    for c in team1.columns[1:]:
-        for i in team1[c].values:
-            if str(i)[0].isalpha() and str(i).lower() != 'nan':
-                team1[c].replace({i: 0}, inplace=True)
-        for i in team2[c].values:
-            if str(i)[0].isalpha() and str(i).lower() != 'nan':
-                team2[c].replace({i: 0}, inplace=True)
+    for column in team1.columns[1:]:
+        for value in team1[column].values:
+            if str(value)[0].isalpha() and str(value).lower() != 'nan':
+                team1[column].replace({value: 0}, inplace=True)
+        for value in team2[column].values:
+            if str(value)[0].isalpha() and str(value).lower() != 'nan':
+                team2[column].replace({value: 0}, inplace=True)
 
     team1.fillna(' ',inplace=True)
     team2.fillna(' ',inplace=True)
@@ -49,6 +49,40 @@ def get_NBA_result(game_id):
     return title,game_result,team1,team2
 
 
+
+def get_NBA_result_with_conditions(game_id):
+    web = requests.get(f'https://www.espn.com/nba/boxscore?gameId={game_id}')
+    content = web.content
+    soup = BeautifulSoup(content,'html.parser')
+    #     get result(score)
+    html = soup.find(name='div',attrs={'class':'competitors'})
+    game_result = pd.read_html(str(html))[0]
+    col = game_result.columns.to_list()
+    col[0] = 'Team'
+    game_result.columns = col
+    #     get article title
+    title = '[BOX] '+game_result['Team'][0]+' '+str(game_result['T'][0])+' : '+str(game_result['T'][1])+' '+game_result['Team'][1]
+    #     get stat(players stat)
+    stat = soup.find(name='div',attrs={'id':'gamepackage-boxscore-module'})
+    for abbr in soup.find_all(name='span',attrs={'class':'abbr'}):
+        abbr.decompose()
+    for position in stat.find_all(name='span',attrs={'class':'position'}):
+        position.insert_before('/')
+
+    team1 = pd.read_html(str(stat))[0]
+    team2 = pd.read_html(str(stat))[1]
+    column_list = team1.columns.tolist()
+    column = []
+    for col_a,col_b in column_list:
+        column.append(col_a)
+    column[0] = 'Players'
+    team1.columns = column
+    team2.columns = column
+
+    team1.fillna(' ',inplace=True)
+    team2.fillna(' ',inplace=True)
+    
+    return title,game_result,team1,team2
 
 
 
